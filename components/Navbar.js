9e5068fca8e941cc7d7c969271b5c176c17b4bc8
bitterkofte <hasantalhahtc@gmail.com>
2023-03-24 11:57:46 +0300

    Draawer completed

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import React, { useState } from 'react'
 import styled from "styled-components";
+// import CV from '../public/assets/HasanTalhaCelik.pdf'
 import { ImCross, ImMenu, ImLinkedin, ImGithub, ImMail4 } from "react-icons/im";
 
 const Navbar = () => {
@@ -13,12 +14,19 @@ const Navbar = () => {
       <NavbarCont>
 
         <NavLeft>
-          <NavLink href={"/"} font={"SofiaBlack"} size={"30px"}>bitterkofte</NavLink>
+          <NavLink href={"/"} font={"SofiaBlack"} size={"30px"} pad={"10px"}>bitterkofte</NavLink>
         </NavLeft>
 
         <NavRight>
-          <NavLink href={"/"}>Contact Me</NavLink>
-          <NavLink href={"/"} bgc={"#cdcd00"} >Download CV</NavLink>
+          <NavLink href={"/"} pad={"10px"}>Contact Me</NavLink>
+          <GoTo href="assets/HasanTalhaCelik.pdf" 
+                alt="download"
+                target="_blank"
+                rel="noopener noreferrer"
+                bgc={"#cdcd00"}
+                pad={"10px"}>
+                Download CV
+          </GoTo>
         </NavRight>
 
         <NavMenu>
@@ -26,40 +34,54 @@ const Navbar = () => {
         </NavMenu>
 
       </NavbarCont>
-      <ExtendedContainer visible={navMenu}>
-      <Extended>
-        <ExtHead>
-          <span style={{color: '#ffffff', fontSize: '30px'}}>Hasan</span>
-          <ImCross onClick={handleNav} className='icon' size={32} color={"#ffffff"}/>
-        </ExtHead>
-        <ExtLinks>
-          <NavLink href={"/"}>Home</NavLink>
-          <NavLink href={"/"}>About</NavLink>
-          <NavLink href={"/"}>Contact Me</NavLink>
-          <NavLink href={"/"}>Download CV</NavLink>
-        </ExtLinks>
-        <ExtFoot>
-          <a
-            href='https://github.com/bitterkofte'
-            target='_blank'
-            rel='noreferrer'>
-            <ImLinkedin size={32} color={"#ffffff"}/>
-          </a>
-          <a
-            href='https://github.com/bitterkofte'
-            target='_blank'
-            rel='noreferrer'>
-            <ImGithub size={32} color={"#ffffff"}/>
-          </a>
-          <a
-            href='hasantalhahtc@gmail.com'
-            target='_blank'
-            rel='noreferrer'>
-            <ImMail4 size={32} color={"#ffffff"}/>
-          </a>
-        </ExtFoot>
-      </Extended>
-      </ExtendedContainer>
+
+      <DarkBG visible={navMenu}>
+        <ExtendedContainer visible={navMenu}>
+        <Extended>
+          <ExtHead>
+            <span style={{color: '#ffffff', fontSize: '30px'}}>Hasan</span>
+            <ImCross onClick={handleNav} className='icon' size={32} color={"#ffffff"}/>
+          </ExtHead>
+          <ExtLinks>
+            <NavLink href={"/"} font={"SofiaBold"} onClick={() => setNavMenu(false)}>Home</NavLink>
+            <NavLink href={"/about"} font={"SofiaBold"} onClick={() => setNavMenu(false)}>About</NavLink>
+            <NavLink href={"/contact"} font={"SofiaBold"} onClick={() => setNavMenu(false)}>Contact Me</NavLink>
+            <GoTo href="assets/HasanTalhaCelik.pdf" 
+                  alt="alt text"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  bgc={"#cdcd00"}
+                  pad={"10px"}
+                  font={"SofiaBold"}>
+                  Download CV
+            </GoTo>
+          </ExtLinks>
+          <ExtFoot>
+            <div className='flex'>
+            <a
+              href='https://github.com/bitterkofte'
+              target='_blank'
+              rel='noreferrer'>
+              <ImLinkedin size={32} color={"#ffffff"}/>
+            </a>
+            <a
+              href='https://github.com/bitterkofte'
+              target='_blank'
+              rel='noreferrer'>
+              <ImGithub size={32} color={"#ffffff"}/>
+            </a>
+            <a
+              href='hasantalhahtc@gmail.com'
+              target='_blank'
+              rel='noreferrer'>
+              <ImMail4 size={32} color={"#ffffff"}/>
+            </a>
+            </div>
+            <div className='copy'>&copy; Copyright 2023 Hasan Çelik. All rights reserved. • Privacy Policy</div>
+          </ExtFoot>
+        </Extended>
+        </ExtendedContainer>
+      </DarkBG>
     </div>
   )
 }
@@ -97,33 +119,41 @@ const NavMenu = styled.div`
     display: none;
   }
 `
+const DarkBG = styled.div`
+  /* display: ${({visible}) => visible ? "block" : "none"}; */
+  /* position: fixed;
+  width: 100%;
+  top: 0;
+  background-color: #00000094;
+  height: 100vh; */
+`
 const ExtendedContainer = styled.div`
-  display: ${({visible}) => visible ? "flex" : "none"};
-  /* flex-direction: column; */
+  /* display: inline-flex; */
   position: fixed;
   top: 0;
-  right: 0;
+  right: ${({visible}) => visible ? "0" : "-100%"};
   height: 100vh;
-  width: 50%;
-  /* padding: 20px; */
+  width: 80%;
   background-color: #006d83;
-  /* border: 10px solid red; */
-  /* justify-content: space-between; */
+  transition-timing-function: cubic-bezier(0.4, 0, 1, 1);
+  transition-duration: 500ms;
 `
 const Extended = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
-  /* height: 100%; */
-  margin: 20px;
+  height: 100%;
+  /* padding: 20px; */
   justify-content: space-between;
+  background-color: #5e2a7b;
 `
 const ExtHead = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
-  border: 1px solid #ff0000;
+  padding: 20px;
+  /* border: 1px solid #ff0000; */
   .icon {
     cursor: pointer;
   }
@@ -131,15 +161,25 @@ const ExtHead = styled.div`
 const ExtLinks = styled.div`
   display: flex;
   flex-direction: column;
-  border: 1px solid #ff0000;
-
+  justify-content: space-evenly;
+  align-items: center;
+  padding: 20px;
+  height: 50%;
 `
 const ExtFoot = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  border: 1px solid #ff0000;
-
+  padding: 20px;
+  .flex {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+  }
+  .copy {
+    margin-top: 10px;
+    color: #ffffff;
+    font-size: 10px;
+    text-align: center;
+  }
 `
 
 const NavLink = styled(Link)`
@@ -148,6 +188,20 @@ const NavLink = styled(Link)`
   background-color: ${({bgc}) => bgc || "#ffffff00"};
   font-family: ${({font}) => font || "Sofia"};
   font-size: ${({size}) => size || "20px"};
-  padding: 10px;
-  
+  padding: ${({pad}) => pad || "0px"};
+  :link {
+    transition: 400ms;
+  }
+  :hover {
+    color: #666666;
+    transition: 200ms;
+  }
+`
+const GoTo = styled.a`
+  text-decoration: none;
+  color: #ffffff;
+  background-color: ${({bgc}) => bgc || "#ffffff00"};
+  font-family: ${({font}) => font || "Sofia"};
+  font-size: ${({size}) => size || "20px"};
+  padding: ${({pad}) => pad || "0px"};
 `
