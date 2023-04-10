57e46683155fbc3b30d6e9250df0514e2b1d53ef
bitterkofte <hasantalhahtc@gmail.com>
2023-04-10 16:01:23 +0300

    Meta

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ import MainPage from '@component/components/MainPage'
 import About from '@component/components/About'
 import Skills from '@component/components/Skills'
 import Projects from '@component/components/Projects'
+import MetaThumbnail from '../public/assets/bk.png'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,16 +19,16 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
 
-        <meta property="og:title" content="bitterkofte - Hasan Talha Çelik"/>
         <meta property="og:type" content="website" />
-        <meta property="og:image" content="../public/assets/bk.svg"/>
+        <meta property="og:title" content="bitterkofte - Hasan Talha Çelik"/>
         <meta property="og:url" content="https://bitterkofte.vercel.app/"/>
+        <meta property="og:image" content={MetaThumbnail}/>
 
         <meta property="twitter:card" content="summary_large_image"/>
         <meta property="twitter:url" content="https://bitterkofte.vercel.app/"/>
         <meta property="twitter:title" content="Hasan Talha Çelik"/>
         <meta property="twitter:description" content="Hey, this is my personal website. You can take a look at my portfolio, download my CV and see how you can contact me!"/>
-        <meta property="twitter:image" content="../public/assets/bk.png"/>
+        <meta property="twitter:image" content={MetaThumbnail}/>
       </Head>
       {/* <MainMenu/> */}
       <MainPage/>
